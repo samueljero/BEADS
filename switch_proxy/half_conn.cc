2062a98a29e2ee47e56fc3f69bf231a206e39e97
Samuel Jero <sjero@purdue.edu>
2015-03-10 23:42:29 -0400

    More work on proxy

--- switch_proxy/half_conn.cc
+++ switch_proxy/half_conn.cc
@@ -69,17 +69,52 @@ void* HalfConn::thread_run(void* arg)
 	return NULL;
 }
 
+struct ofp_header{
+	uint8_t version;
+	uint8_t type;
+	uint16_t length;
+};
+
 void HalfConn::run()
 {
 	char *buff;
 	int blen;
 	int len;
+	int mlen;
 	Message m;
+	struct ofp_header *hdr;
 
-	while(true) {
-		if ((len = recv(sock,buff,blen,len,0) < 0) {
+	while (true) {
+		mlen = blen = 65535;
+		m.len = 0;
+		buff = m.buff = (char*)malloc(blen);
+		if (!buff) {
+			dbgprintf(0, "Error: Cannot allocate Memory!\n");
+			close(sock);
+			sock = 0;
+			return;
+		}
+		
+		/* Receive Message */
+		while (mlen > m.len) {
+			if ((len = recv(sock,buff,blen,0)) < 0) {
+				dbgprintf(0, "Error: recv() failed: %s\n", strerror(errno));
+				close(sock);
+				sock = 0;
+				return;
+			}
 
+			if (m.len < 4 && len >= 4) { /* find the length in the OpenFlow header */
+				hdr = (struct ofp_header*)m.buff;
+				mlen = ntohs(hdr->length);
+			}
+			buff += len;
+			m.len += len;
+			blen -= len;
 		}
 
+		/* Send message */
+		other->sendm(m);
+		free(m.buff);
 	}
 }
